681dec5d29c893a733197c43c099ca28dceabc01
Tejas Nikam <Tejasnikam4515@gmail.com>
2023-04-12 23:31:38 +0530

    Error handled in api

--- src/components/Meals/AvalibleMeals.jsx
+++ src/components/Meals/AvalibleMeals.jsx
@@ -1,19 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import Card from "../UI/Card";
-import MealItem from "./MealItem/MealItem";
-import classes from "./AvalibleMeals.module.css";
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvalibleMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
-    
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://food-app-f7a33-default-rtdb.firebaseio.com/meals.json"
+        'https://food-app-f7a33-default-rtdb.firebaseio.com/meals.json'
       );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -28,16 +33,29 @@ const AvailableMeals = () => {
       }
 
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchMeals();
-    setIsLoading(false);
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
-  if(isLoading){
-    return <section className={classes.MealsLoading}>
-      <p>Loading...</p>
-    </section>
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
   }
 
   const mealsList = meals.map((meal) => (
